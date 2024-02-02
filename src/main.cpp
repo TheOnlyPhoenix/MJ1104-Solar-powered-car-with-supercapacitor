ad5cdb337c2e0dc8df1e95b0628729ebbdfab803
TheOnlyPhoenix <fragmented.phoenix@gmail.com>
2024-02-02 14:31:02 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ int motor_2_ctrl_2 = 5;
 
 void setup() {
   pinMode(motor_1_ctrl_1, OUTPUT);
+  pinMode(motor_1_ctrl_2, OUTPUT);
+  pinMode(motor_2_ctrl_1, OUTPUT);
+  pinMode(motor_2_ctrl_2, OUTPUT);
 }
 
 void loop() {
