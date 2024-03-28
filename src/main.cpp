5c3333244b1c1c846d4687723b5b5dc8dffd09c4
TheOnlyPhoenix <fragmented.phoenix@gmail.com>
2024-03-28 15:47:49 +0100

    update sensor code
    now has proper code for two sensors and two motors

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,12 @@ int motor1Pin1 = 2;
 int motor1Pin2 = 3;
 int motor2Pin1 = 4;
 int motor2Pin2 = 5;
-
+int lineSensor1 = 8;
+int lineSensor2 = 9;
 
 void setup() {
+  pinMode(lineSensor1, INPUT);
+  pinMode(lineSensor2, INPUT);
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
   pinMode(motor2Pin1, OUTPUT);
@@ -13,17 +16,32 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
+  if (digitalRead(lineSensor1)==HIGH && digitalRead(lineSensor2)==HIGH) {
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+  }
+  else if(digitalRead(lineSensor1)==LOW) {
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
 
-  // digitalWrite(motor2Pin1, HIGH);
-  // digitalWrite(motor2Pin2, LOW);
-
-
-  // digitalWrite(motor1Pin1, LOW);
-  // digitalWrite(motor1Pin2, HIGH);
-
-  // // digitalWrite(motor2Pin1, LOW);
-  // // digitalWrite(motor2Pin2, HIGH);
-  // delay(1000);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+  }
+  else if (digitalRead(lineSensor2)==LOW) {
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+  
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+  }
+  else {
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+  }
 }
